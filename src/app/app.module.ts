226b1814d963506ab22847d9e9b1dc7b99c15550
Will Holcomb <will@dhappy.org>
2019-09-29 21:25:03 -0400

    ng g c components/Leg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LegComponent } from './components/leg/leg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LegComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/leg/leg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leg',
+  templateUrl: './leg.component.html',
+  styleUrls: ['./leg.component.scss']
+})
+export class LegComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
