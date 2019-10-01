41ba6095094c80686ea780033d33e88d089279a5
Will Holcomb <will@dhappy.org>
2019-10-01 16:06:10 -0400

    working on teams

--- src/app/models/Leg.ts
+++ src/app/models/Leg.ts
@@ -1,6 +1,7 @@
 export class Leg {
   id:string
   path:string
+  icon:any
 
   constructor(base?:any) {
     Object.assign(this, base)
--- src/app/models/Team.ts
+++ src/app/models/Team.ts
@@ -2,6 +2,10 @@ export class Team {
   id:string
   path:string
   icon:any
+  x:number
+  y:number
+  width:number
+  height:number
 
   constructor(base?:any) {
     Object.assign(this, base)
